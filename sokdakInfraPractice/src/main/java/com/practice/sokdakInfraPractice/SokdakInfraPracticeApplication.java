c5ec94fa951076bc5f5b264ccfbe2dee99e8eec8
hyunrrr <whgusrms96@gmail.com>
2022-08-18 11:26:56 +0900

    chore: 스프링 프로젝트 추가

---
+++ sokdakInfraPractice/src/main/java/com/practice/sokdakInfraPractice/SokdakInfraPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.sokdakInfraPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokdakInfraPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokdakInfraPracticeApplication.class, args);
+	}
+
+}
