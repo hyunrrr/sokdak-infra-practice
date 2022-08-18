dcc4d9172d948e58e99bfe585e1a92f2c0b592dc
hyunrrr <whgusrms96@gmail.com>
2022-08-18 11:47:56 +0900

    chore: 간단한 스프링 프로젝트

---
+++ sokdakInfraPractice/src/test/java/com/practice/TempTest.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TempTest {
+
+    @DisplayName("임시 테스트")
+    @Test
+    void test() {
+    }
+}
