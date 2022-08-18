df314f8519f2c1082a8e94bc01d9ac176c9031f6
hyunrrr <whgusrms96@gmail.com>
2022-08-18 14:57:02 +0900

    로컬데이트타임 테스트

--- sokdakInfraPractice/src/test/java/com/practice/TempTest.java
+++ sokdakInfraPractice/src/test/java/com/practice/TempTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.LOCAL_DATE_TIME;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import net.bytebuddy.asm.Advice.Local;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,8 +31,8 @@ class TempTest {
     @DisplayName("timezone 확인 테스트")
     @Test
     void test4() {
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(now);
-        assertThat(now).isEqualTo(LocalDateTime.of(01, 01, 01, 01, 01));
+        String formatDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH"));
+        System.out.println(formatDate);
+        assertThat(formatDate).isEqualTo(LocalDateTime.now().format(DateTimeFormatter.ofPattern("2022-08-18-14")));
     }
 }
